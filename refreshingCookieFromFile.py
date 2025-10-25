dd0a67cc37ad1c976a231ea9f10c4691c77ab6c0
unknown <abd.allatif.aleddah.99@gmail.com>
2025-10-25 11:52:38 +0300

    Refresh cookie from kookie.txt3.

--- refreshingCookieFromFile.py
+++ refreshingCookieFromFile.py
@@ -18,34 +18,11 @@ class RefreshingCookieFromFileThread(Thread):
                     cookie = f.readline().replace("\n","").strip()
                     if config.telegram.COOKIE_STRING != cookie:
                         config.telegram.COOKIE_STRING = cookie
-                    
-                    print(cookie)
-                    print("###################################")
-                    print(config.telegram.COOKIE_STRING)
             except:
                 self.logger.info("there is an error in refreshing cookie from file")
                 print("there is an error in refreshing cookie from file")
             time.sleep(10)
-        api = iChancyAPI()
-        result = api.checkCookieIsWork()
-        self.logger.info(f"API FOR REFRESH COOKIE WITH RESULT {result.get('success')}" + "    " + f"{result.get('error')}")     
-        config.telegram.COOKIE_MESSAGE_SENT = False
-        while True:
-            oldCookieTime = config.telegram.UPDATE_COOKIE_DATE
-            cookieExpired = oldCookieTime + 1780.0
-            if datetime.timestamp(datetime.now()) > cookieExpired :
-                api = iChancyAPI()
-                result = api.checkCookieIsWork()
-                self.logger.info(f"API FOR REFRESH COOKIE WITH RESULT {result.get('success')}" + "    " + f"{result.get('error')}")    
-                config.telegram.COOKIE_MESSAGE_SENT = False
-                print(cookieExpired)
-                print(oldCookieTime)
-                time.sleep(5)
-            
-            if int(datetime.timestamp(datetime.now()))%100 == 0 or int(datetime.timestamp(datetime.now()))%100 == 3 or int(datetime.timestamp(datetime.now()))%100 == 2:
-                        config.telegram.COOKIE_MESSAGE_SENT = False
-                        print(config.telegram.COOKIE_MESSAGE_SENT)
-            time.sleep(3)
+    
 
 
 
